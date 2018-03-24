172132384b1c56135721d4caf0a024af86110d83
Richie Messina <messinar@8c859041b775.ant.amazon.com>
2018-03-23 18:52:52 -0700

    comments

--- zshrc.zsh
+++ zshrc.zsh
@@ -1,18 +1,13 @@
-# Theme to load
+### oh-my-zsh Theme ###
 # ZSH_THEME="dracula"
 
-# Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-# Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
+### oh-my-zsh Plugins ###
 # plugins=(
 #     history-substring-search
 #     colored-man-pages
 #     vi-mode
 #     tmux
 # )
-
-# source $ZSH/oh-my-zsh.sh
 
 ### Functions ###
 # ls whenever cd
